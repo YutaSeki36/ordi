18c648ca3d880d48ad0469fa14b9a1453a582602
YutaSeki36 <sekiyuta0306@gmail.com>
2019-01-04 02:18:15 +0900

    first commit

---
+++ src/main/java/app/ysek/note/ordi/OrdiApplication.java
@@ -0,0 +1,14 @@
+package app.ysek.note.ordi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdiApplication.class, args);
+	}
+
+}
+
